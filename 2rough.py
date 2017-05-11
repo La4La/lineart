b534a0a474c17bbc4fd65c99c7ef5fcc1757d80a
La4La <gaozhengyan0110@gmail.com>
2017-05-11 16:49:39 +0900

    Add files via upload

--- 2rough.py
+++ 2rough.py
@@ -2,6 +2,11 @@
 import cv2
 import numpy as np
 import random
+import argparse
+import os
+import sys
+import subprocess
+
 np.set_printoptions(threshold=np.inf)
 
 def aff(img, local=True):
@@ -12,13 +17,16 @@ def aff(img, local=True):
         for i in range(a):
             img_cp = 1.0 - img / 255.0
             # affine transformation
-            rad = np.pi * random.uniform(-0.03, 0.03)
             if random.random() > 0.7:
-                move_x = random.uniform(-1.5,1.5)
+                rad = np.pi * random.uniform(-0.03, 0.03)
+            else:
+                rad = 0
+            if random.random() > 0.5:
+                move_x = random.uniform(-2,2)
             else:
                 move_x = 0
-            if random.random() > 0.7:
-                move_y = random.uniform(-1.5,1.5)
+            if random.random() > 0.5:
+                move_y = random.uniform(-2,2)
             else:
                 move_y = 0
             afn_M = np.float32([[np.cos(rad), -1 * np.sin(rad), move_x],
@@ -30,13 +38,16 @@ def aff(img, local=True):
             if random.random() < 0.6:
                 img_cp = 1.0 - img / 255.0
                 # affine transformation
-                rad = np.pi * random.uniform(-0.08, 0.08)
-                if random.random() > 0.3:
-                    move_x = random.uniform(-4.5, 4.5)
+                if random.random() > 0.5:
+                    rad = np.pi * random.uniform(-0.08, 0.08)
+                else:
+                    rad = 0
+                if random.random() > 0.4:
+                    move_x = random.uniform(-5,5)
                 else:
                     move_x = 0
-                if random.random() > 0.3:
-                    move_y = random.uniform(-4.5, 4.5)
+                if random.random() > 0.4:
+                    move_y = random.uniform(-5,5)
                 else:
                     move_y = 0
                 afn_M = np.float32([[np.cos(rad), -1 * np.sin(rad), move_x],
@@ -50,7 +61,7 @@ def addPointNoise(img):
     rows, cols = img.shape
     row = random.randint(0, rows - 1)
     col = random.randint(0, cols - 1)
-    for j in range(random.randint(4, 25)):
+    for j in range(random.randint(4, 35)):
         #print(row, col)
         img[row, col] = 0
         patern = random.randint(1, 8)
@@ -94,7 +105,7 @@ def dilation(img, glob=False):
     return img
 
 def erosion(img):
-    n = random.randint(1,3)
+    n = random.randint(1,2)
     neibor = np.ones((n,n), np.uint8)
     img = cv2.erode(img, neibor, iterations = 1)
     return img
@@ -103,113 +114,122 @@ def addDirt(img):
     rows, cols = img.shape
     Dirt = np.ones(img.shape, np.uint8) * 255
     change = 0
-    for i in range(random.randint(0,5)):
+    for i in range(random.randint(0,3)):
         change = 1
         y = random.randint(0,rows-1)
         x = random.randint(0,cols-1)
         dy = random.randint(rows//30,rows//5)
         dx = random.randint(cols//30,cols//5)
-        Dirt[y:min(y+dy,rows-1), x:min(x+dx,cols-1)] =  random.randint(200,230)
-    k_size = random.randint(70,80) * 2 + 1
+        Dirt[y:min(y+dy,rows-1), x:min(x+dx,cols-1)] =  random.randint(215,230)
+    k_size = random.randint(max(rows,cols)//18,max(rows,cols)//14) * 2 + 1
     Dirt = cv2.GaussianBlur(Dirt, (k_size, k_size), 0)
     if change:
-        img = np.where(img < 220, img, Dirt)
+        img = np.where(img < 230, img, Dirt)
     return img
 
 
-img = cv2.imread('line.jpg', 0)
-rows, cols = img.shape
-zero = np.zeros(img.shape)
+def make_rough(src):
+    img = cv2.imread(src, 0)
+    rows, cols = img.shape
+    zero = np.zeros(img.shape, np.uint8)
 
-# apply affine transformation
-itv = random.randint(25,35) #interval
-i_n = rows//itv
-j_n = cols//itv
-for i in range(i_n-1):
-    for j in range(j_n-1):
-        zero[i*itv:(i+1)*itv, j*itv:(j+1)*itv] = aff(img[i*itv:(i+1)*itv, j*itv:(j+1)*itv])
-    zero[i*itv:(i+1)*itv, (j_n-1)*itv:cols] = aff(img[i*itv:(i+1)*itv, (j_n-1)*itv:cols])
-for j in range(j_n):
-    zero[(i_n-1)*itv:rows, j*itv:(j+1)*itv] = aff(img[(i_n-1)*itv:rows, j*itv:(j+1)*itv])
-zero[(i_n-1)*itv:rows, (j_n-1)*itv:cols] = aff(img[(i_n-1)*itv:rows, (j_n-1)*itv:cols])
+    # apply affine transformation
+    itv = random.randint(min(rows,cols)//30,min(rows,cols)//20) #interval
+    i_n = rows//itv
+    j_n = cols//itv
+    for i in range(i_n-1):
+        for j in range(j_n-1):
+            zero[i*itv:(i+1)*itv, j*itv:(j+1)*itv] = aff(img[i*itv:(i+1)*itv, j*itv:(j+1)*itv])
+        zero[i*itv:(i+1)*itv, (j_n-1)*itv:cols] = aff(img[i*itv:(i+1)*itv, (j_n-1)*itv:cols])
+    for j in range(j_n):
+        zero[(i_n-1)*itv:rows, j*itv:(j+1)*itv] = aff(img[(i_n-1)*itv:rows, j*itv:(j+1)*itv])
+    zero[(i_n-1)*itv:rows, (j_n-1)*itv:cols] = aff(img[(i_n-1)*itv:rows, (j_n-1)*itv:cols])
 
-# apply larger affine transformation
-itv = random.randint(35,60) #interval
-i_n = rows//itv
-j_n = cols//itv
-for i in range(i_n-1):
-    for j in range(j_n-1):
-        zero[i*itv:(i+1)*itv, j*itv:(j+1)*itv] = aff(zero[i*itv:(i+1)*itv, j*itv:(j+1)*itv], local=False)
-    zero[i*itv:(i+1)*itv, (j_n-1)*itv:cols] = aff(zero[i*itv:(i+1)*itv, (j_n-1)*itv:cols], local=False)
-for j in range(j_n):
-    zero[(i_n-1)*itv:rows, j*itv:(j+1)*itv] = aff(zero[(i_n-1)*itv:rows, j*itv:(j+1)*itv], local=False)
-zero[(i_n-1)*itv:rows, (j_n-1)*itv:cols] = aff(zero[(i_n-1)*itv:rows, (j_n-1)*itv:cols], local=False)
-#cv2.imshow('zero', zero/255)
-#cv2.waitKey(0)
+    # apply larger affine transformation
+    itv = random.randint(min(rows,cols)//20,min(rows,cols)//15) #interval
+    i_n = rows//itv
+    j_n = cols//itv
+    for i in range(i_n-1):
+        for j in range(j_n-1):
+            zero[i*itv:(i+1)*itv, j*itv:(j+1)*itv] = aff(zero[i*itv:(i+1)*itv, j*itv:(j+1)*itv], local=False)
+        zero[i*itv:(i+1)*itv, (j_n-1)*itv:cols] = aff(zero[i*itv:(i+1)*itv, (j_n-1)*itv:cols], local=False)
+    for j in range(j_n):
+        zero[(i_n-1)*itv:rows, j*itv:(j+1)*itv] = aff(zero[(i_n-1)*itv:rows, j*itv:(j+1)*itv], local=False)
+    zero[(i_n-1)*itv:rows, (j_n-1)*itv:cols] = aff(zero[(i_n-1)*itv:rows, (j_n-1)*itv:cols], local=False)
 
-# dilation & erosion
-itv = random.randint(55,80) #interval
-i_n = rows//itv
-j_n = cols//itv
-for i in range(i_n-1):
-    for j in range(j_n-1):
-        if random.random() < 0.2:
-            zero[i*itv:(i+1)*itv, j*itv:(j+1)*itv] = dilation(zero[i*itv:(i+1)*itv, j*itv:(j+1)*itv])
-        elif random.random() > 0.66:
-            zero[i*itv:(i+1)*itv, j*itv:(j+1)*itv] = erosion(zero[i*itv:(i+1)*itv, j*itv:(j+1)*itv])
-    if random.random() < 0.2:
-        zero[i*itv:(i+1)*itv, (j_n-1)*itv:cols] = dilation(zero[i*itv:(i+1)*itv, (j_n-1)*itv:cols])
-    elif random.random() > 0.66:
-        zero[i*itv:(i+1)*itv, (j_n-1)*itv:cols] = erosion(zero[i*itv:(i+1)*itv, (j_n-1)*itv:cols])
-for j in range(j_n):
-    if random.random() < 0.2:
-        zero[(i_n-1)*itv:rows, j*itv:(j+1)*itv] = dilation(zero[(i_n-1)*itv:rows, j*itv:(j+1)*itv])
-    elif random.random() > 0.66:
-        zero[(i_n-1)*itv:rows, j*itv:(j+1)*itv] = erosion(zero[(i_n-1)*itv:rows, j*itv:(j+1)*itv])
-if random.random() < 0.2:
-    zero[(i_n-1)*itv:rows, (j_n-1)*itv:cols] = dilation(zero[(i_n-1)*itv:rows, (j_n-1)*itv:cols])
-elif random.random() > 0.66:
-    zero[(i_n-1)*itv:rows, (j_n-1)*itv:cols] = erosion(zero[(i_n-1)*itv:rows, (j_n-1)*itv:cols])
-#cv2.imshow('zero', zero/255)
-#cv2.waitKey(0)
+    # dilation & erosion
+    itv = random.randint(55,80) #interval
+    i_n = rows//itv
+    j_n = cols//itv
+    for i in range(i_n-1):
+        for j in range(j_n-1):
+            if random.random() < 0.1:
+                zero[i*itv:(i+1)*itv, j*itv:(j+1)*itv] = dilation(zero[i*itv:(i+1)*itv, j*itv:(j+1)*itv])
+            elif random.random() > 0.8:
+                zero[i*itv:(i+1)*itv, j*itv:(j+1)*itv] = erosion(zero[i*itv:(i+1)*itv, j*itv:(j+1)*itv])
+        if random.random() < 0.1:
+            zero[i*itv:(i+1)*itv, (j_n-1)*itv:cols] = dilation(zero[i*itv:(i+1)*itv, (j_n-1)*itv:cols])
+        elif random.random() > 0.8:
+            zero[i*itv:(i+1)*itv, (j_n-1)*itv:cols] = erosion(zero[i*itv:(i+1)*itv, (j_n-1)*itv:cols])
+    for j in range(j_n):
+        if random.random() < 0.1:
+            zero[(i_n-1)*itv:rows, j*itv:(j+1)*itv] = dilation(zero[(i_n-1)*itv:rows, j*itv:(j+1)*itv])
+        elif random.random() > 0.8:
+            zero[(i_n-1)*itv:rows, j*itv:(j+1)*itv] = erosion(zero[(i_n-1)*itv:rows, j*itv:(j+1)*itv])
+    if random.random() < 0.1:
+        zero[(i_n-1)*itv:rows, (j_n-1)*itv:cols] = dilation(zero[(i_n-1)*itv:rows, (j_n-1)*itv:cols])
+    elif random.random() > 0.8:
+        zero[(i_n-1)*itv:rows, (j_n-1)*itv:cols] = erosion(zero[(i_n-1)*itv:rows, (j_n-1)*itv:cols])
 
-if random.random() < 0.3:
-    # dilation
-    zero = dilation(zero, glob=True)
-elif random.random() > 0.8:
-    # erosion
-    zero = erosion(zero)
-#cv2.imshow('erosion', zero/255)
-#cv2.waitKey(0)
 
-# add point noise
-for i in range(random.randint(30,50)):
-    addPointNoise(zero)
+    if random.random() < 0.15:
+        # dilation
+        zero = dilation(zero, glob=True)
+    elif random.random() > 0.85:
+        # erosion
+        zero = erosion(zero)
 
-# reduce contrast
-min_table = random.randint(0,150)
-max_table = 255
-diff_table = max_table - min_table
-look_up_table = np.arange(256, dtype='uint8')
-for i in range(0, 255):
-    look_up_table[i] = min_table + i * (diff_table) / 255
-img = cv2.LUT(zero.astype(np.uint8), look_up_table)
+    # add point noise
+    for i in range(random.randint(25,40)):
+        addPointNoise(zero)
 
-# blur
-k_size = random.randint(1, 3) * 2 + 1
-blur = cv2.GaussianBlur(img, (k_size, k_size), 0)
-#img = np.where(img < 180, img, blur)
-img = blur
+    # blur
+    k_size = random.randint(0, 2) * 2 + 1
+    blur = cv2.GaussianBlur(zero, (k_size, k_size), 0)
+    img = blur
 
-# add dirt
-img = addDirt(img)
+    # add dirt
+    img = addDirt(img)
 
-# background
-#img = np.where(img < 250, img, random.randint(245,255))
+    # reduce contrast
+    min_table = random.randint(0,150)
+    max_table = 255
+    diff_table = max_table - min_table
+    look_up_table = np.arange(256, dtype='uint8')
+    for i in range(0, 255):
+        look_up_table[i] = min_table + i * (diff_table) / 255
+    img = cv2.LUT(img.astype(np.uint8), look_up_table)
 
-# add Gaussian Noise
-img = addGaussianNoise(img)
+    # add Gaussian Noise
+    img = addGaussianNoise(img)
+
+    return img
 
 
 
-cv2.imwrite('img_out.jpg', img)
+parser = argparse.ArgumentParser(description='change line art to rough sketch')
+parser.add_argument('directory', help='Path to line art files')
+args = parser.parse_args()
+
+def cmd(cmd):
+    return subprocess.getoutput(cmd)
+
+if __name__ == '__main__':
+    os.mkdir("rough")
+    dirs = cmd('ls ' + args.directory)
+
+    images = dirs.splitlines()
+    for i in images:
+        print(i)
+        rough = make_rough(args.directory + '/' + i)
+        cv2.imwrite('rough/' + i, rough)
